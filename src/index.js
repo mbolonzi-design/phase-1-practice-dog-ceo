f5e47862273f5efca616f32ab32ad2963eba5860
mbolonzi-design <mbolonzi.design@gmail.com>
2022-06-30 12:20:43 +0300

    commit tree f851199eb62cb5fd6e4a84c7ce016f293decdbe1

--- src/index.js
+++ src/index.js
@@ -1,52 +1,69 @@
 console.log('%c HI', 'color: firebrick')
 
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    fetchImage()
-    fetchBreeds()
+document.addEventListener('DOMContentLoaded', function(){
+    loadImage();
 })
-function renderDogImages(pic) {
-    let img = document.createElement('img')
-    img.src = pic
-    document.querySelector("#dog-image-container").append(img)
-}
-function fetchImage() {
+
+const loadImage = ()=>{
     const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
     fetch(imgUrl)
-        .then(res => res.json())
-        .then(data => data.message.forEach(renderDogImages))
+    .then(result => result.json())
+    .then(results => {
+        results.message.forEach(image => {
+            addImage(image)
+        });
+    })
 }
-function fetchBreeds(filter) {
+
+const addImage=(pickedUrl)=>{
+    let mainContainer = document.querySelector('#dog-image-container')
+    let imgElem = document.createElement('img')
+    imgElem.src =  pickedUrl
+    mainContainer.appendChild(imgElem)
+}
+
+
+const loadBreeds = () =>{
     const breedUrl = 'https://dog.ceo/api/breeds/list/all'
     fetch(breedUrl)
-        .then(res => res.json())
-        .then(data => {
-            let allBreeds = Object.keys(data.message)
-            let select = document.querySelector('#dog-breeds')
-            select.textContent = ''
-            if (filter){
-                allBreeds = allBreeds.filter(elem => elem[0] === filter)
-            }
-            return allBreeds.forEach(renderDogBreed)
-        })
-            
+    .then(resp => resp.json())
+    .then(breeds =>{
+        updateBreed(breeds)
+        addBreedEventListener()
+    })  
 }
 
-//if there a filter arg, then filter by initial letter 
-// run forEach on newly filtered array 
-function renderDogBreed(breed) {
+const updateBreed= breeds =>{
+    let ul = document.querySelector('#dog-breeds')
+    removeChild(ul)
+    breeds.forEach(breed => addBreed(breed))
+}
+
+const removeChildren =element =>{
+    let child = element.lastElementChild;
+    while(child){
+        element.removeChild()
+        child = element.lastElementChild
+    }
+}
+const addBreedEventListener = () =>{
+    let dropDown = document.querySelector('#breed-dropdown')
+    dropDown.addEventListener('change', function(event){
+        selectedBreed(event.target.value)
+    })
+}
+
+const selectedBreed = () =>{
+    updateBreed(breeds.filter(breed.startWith(letter)))
+}
+const addBreed = () =>{
+    let ul = document.querySelector('#dog-breeds')
     let li = document.createElement('li')
-    li.textContent = breed
-    
-    const select = document.querySelector('#breed-dropdown')
-    select.addEventListener('change', (elem)=> {
-        fetchBreeds(elem.target.value)
-    })
-
-
-    document.querySelector('#dog-breeds').append(li)
-    li.addEventListener("click", () => {
-        li.style.color = "red"
-    })
+    li.innerText = breed
+    li.style.cursor = 'pointer'
+    ul.appendChild(li)
+    addBreedEventListener('click', updateColor)
+}
+const updateColor = event =>{
+    event.target.style.color = 'palevioletred'
 }
